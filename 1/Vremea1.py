8291fc57ef0ca316d67c49f620e82f47a9bf2f4d
biancasandu <36482349+biancasandu@users.noreply.github.com>
2018-09-19 17:24:16 +0200

    error
    error code in caz de nume gresit

--- 1/Vremea1.py
+++ 1/Vremea1.py
@@ -1,9 +1,16 @@
 import requests
 
 city= raw_input("Enter the city name: ")
+city= city.title()
 url="https://api.openweathermap.org/data/2.5/weather?q={}&appid=21ff405208e1a89176374d0517de0ef7&units=metric".format(city)
 info=requests.get(url)
 read=info.json()
-print "City name:{}".format(read['name'])
-print "Temperature:{}C".format(read['main']['temp'])
-print "Humidity:{}".format(read['main']['humidity'])
+while True:
+    try:
+        print "City name:{}".format(read['name'])
+        print "Temperature:{}C".format(read['main']['temp'])
+        print "Humidity:{}".format(read['main']['humidity'])
+        break
+    except KeyError:
+        print "That city does not exist in our database "
+        break
